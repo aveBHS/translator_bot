5a4bcbb7f6d512ecc2d7063d50ff7416c21335aa
aveBHS <aveBHS.ru@gmail.com>
2020-01-08 18:42:59 +0300

    Новые функции в боте
    Теперь можно определять язык по тексту и переводить текст

--- bot.py
+++ bot.py
@@ -19,11 +19,22 @@ def detect_language(text):
         print(response)
         print(response.url)
 
+def language_name(ui):
+    args = {'key': config.translator_token, 'ui': ui}
+    response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/getLangs', params=args)
+    if(response.status_code == 200):
+        return response.json()['langs'][ui]
+    else:
+        print(response)
+        print(response.url)
+
 def translate(text, lang):
     args = {'key': config.translator_token, 'text': text, 'lang': lang}
     response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate', params=args)
     if(response.status_code == 200):
         return response.json()['text'][0]
+    elif(response.status_code == 400):
+        return -1
     else:
         print(response)
         print(response.url)
@@ -31,12 +42,34 @@ def translate(text, lang):
 for event in longpoll.listen():
     if(event.type == VkBotEventType.MESSAGE_NEW):
         text = event.object.text
-        if(len(text) < 500):
+        words = event.object.text.lower().split(' ')
+        if(words[0] == '/language' or words[0] == '/lang' or words[0] == '/язык' or words[0] == '/определи'):
+            lang = detect_language(text)
+            lang_name = language_name(lang)
+            write_msg(f'Язык определен, как: {lang_name}\nКод языка: {lang}', event.object.peer_id)
+        if(words[0] == '/trans' or words[0] == '/translate' or words[0] == '/перевод' or words[0] == '/переведи'):
+            if(len(words) < 3):
+                write_msg(f'Используйте команду в виде: {words[0]} [код языка] [текст]\nПример: {words[0]} en Привет!', event.object.peer_id)
+                continue
+            text_to_translate = ''
+            i = 0
+            for word in words:
+                if(i < 2):
+                    i += 1
+                else:
+                    text_to_translate += word + ' '
+            translated_text = translate(text_to_translate, words[1])
+            if(translated_text == -1):
+                write_msg(f'Вы ввели неверный код языка!\nИспользуйте команду в виде: {words[0]} [код языка] [текст]\nПример: {words[0]} en Привет!', event.object.peer_id)
+                continue
+            else: 
+                write_msg(f'Перевод успешно завершен:\n{translated_text}', event.object.peer_id)
+        elif(len(text) < 500):
             lang = detect_language(text)
             if(lang != 'ru'):
                 if(len(text) < 101):
                     translated_text = translate(text, 'ru')
-                    write_msg(f'В сообщении обнаружен иностранный текст ({lang}). Перевод на русский:\n{translated_text}', event.object.peer_id)
+                    write_msg(f'В сообщении обнаружен иностранный текст ({lang}).\nПеревод на русский:\n{translated_text}', event.object.peer_id)
                 else:
                     write_msg(f'В сообщении обнаружен иностранный текст ({lang}), начинаю переводить...', event.object.peer_id)
                     translated_text = translate(text, 'ru')
